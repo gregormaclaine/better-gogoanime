73fcd9c26487d451b1407f237637deb308c9b682
Gregor Maclaine <thegregormaclaine@gmail.com>
2022-04-10 05:18:32 +0100

    Added env

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = 1000;
+const { PORT } = require('./config');
 const fetch = require('node-fetch');
 
 app.use(require('cors')());
@@ -10,12 +10,16 @@ function get_url(page) {
     return `${GOGOANIME_URL}/page-recent-release.html?page=${page}&type=1`;
 }
 
+app.get('/', (req, res) => {
+    res.send('Better GoGoAnime - Server');
+})
+
 app.get('/page/:page', async (req, res) => {
     const page = await fetch(get_url(parseInt(req.params.page)));
     const text = await page.text();
     res.send(text);
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
 });
